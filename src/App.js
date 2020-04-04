0b2261b944c08c346e71d151146cba7e829f536c
itsamanverma <amanvermame786@gmail.com>
2020-04-04 18:59:26 +0530

    manipulate the list of data & rendering the DOM using the ES6 features

--- src/App.js
+++ src/App.js
@@ -3,87 +3,88 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  state = {
-    persons : [
-      { id:1, name: "aman", age: 25},
-      { id:2, name: "sona" , age:24},
-      { id:3, name: "mini" , age:24}
-    ]
-  }
 
-  nameChangeHandler = (event, id) => {
+    state = {
+      persons : [
+        { id:1, name: "aman", age: 25},
+        { id:2, name: "sona" , age:24},
+        { id:3, name: "mini" , age:24}
+      ]
+    }
 
-    const personIndex = this.state.persons.findIndex( p => {
-      return p.id === id;
-    });
+    nameChangeHandler = (event, id) => {
 
-    const person = {
-      ...this.state.persons[personIndex]
-    };
+      const personIndex = this.state.persons.findIndex( p => {
+        return p.id === id;
+      });
 
-    //alternative ways 
-    // const person = Object.assign({}, this.state.persons[personIndex]);
+      const person = {
+        ...this.state.persons[personIndex]
+      };
 
-    person.name = event.target.value;
+      //alternative ways 
+      // const person = Object.assign({}, this.state.persons[personIndex]);
 
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
+      person.name = event.target.value;
 
-    this.setState ( {
-      persons: persons })
-  }
+      const persons = [...this.state.persons];
+      persons[personIndex] = person;
+
+      this.setState ( {
+        persons: persons })
+    }
   
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice();//slice method copy the whole array into assign var;
-    const persons =  [...this.state.persons];
-    persons.splice(personIndex,1);
-    this.setState( { persons: persons} );
-  }
+    deletePersonHandler = (personIndex) => {
+      // const persons = this.state.persons.slice();//slice method copy the whole array into assign var;
+      const persons =  [...this.state.persons];
+      persons.splice(personIndex,1);
+      this.setState( { persons: persons} );
+    }
 
-  togglePersonHandler = () => {
-    const doesShow = this.state.showPerson;
-    this.setState({showPerson: !doesShow});
-  }
+    togglePersonHandler = () => {
+      const doesShow = this.state.showPerson;
+      this.setState({showPerson: !doesShow});
+    }
 
-render() {
-  const style = {
-    backgroundColor: 'white',
-    font:'inherit',
-    border:'2px solid blue',
-    padding:'5px',
-    cursor:'pointer' 
-  };
+    render() {
+      const style = {
+        backgroundColor: 'white',
+        font:'inherit',
+        border:'2px solid blue',
+        padding:'5px',
+        cursor:'pointer' 
+      };
 
-      let persons = null;
+          let persons = null;
 
-      if(this.state.showPerson){
-        persons = (
-          <div>
-            {this.state.persons.map( (person, index) => {
-              return <Person 
-                  click={() => this.deletePersonHandler(index)}
-                  name ={person.name}
-                  age = {person.age} 
-                  key = {person.id}
-                  changed = {(event) => this.nameChangeHandler(event, person.id)} />
-            })} 
-          </div>
-        );
+          if(this.state.showPerson){
+            persons = (
+              <div>
+                {this.state.persons.map( (person, index) => {
+                  return <Person 
+                      click={() => this.deletePersonHandler(index)}
+                      name ={person.name}
+                      age = {person.age} 
+                      key = {person.id}
+                      changed = {(event) => this.nameChangeHandler(event, person.id)} />
+                })} 
+              </div>
+            );
+          }
+          return (
+            <div className="App">
+              <h1>hi, I am  create the react App </h1>
+              <p> this is actually working...!!</p>
+              <button style= {style} 
+              onClick={this.togglePersonHandler} >toggle Person</button>
+              {persons}
+            </div>
+          );
+
+        // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, "Does this work now?"), React.createElement('p', {className: 'App'}, 'AMAN VERMA creating the react app'))
+
       }
-      return (
-        <div className="App">
-          <h1>hi, I am  create the react App </h1>
-          <p> this is actually working...!!</p>
-          <button style= {style} 
-          onClick={this.togglePersonHandler} >toggle Person</button>
-          {persons}
-        </div>
-      );
+    }
 
-    // return React.createElement('div', null, React.createElement('h1', {className: 'App'}, "Does this work now?"), React.createElement('p', {className: 'App'}, 'AMAN VERMA creating the react app'))
-
-  }
-}
-
-export default App;
+    export default App;
  
