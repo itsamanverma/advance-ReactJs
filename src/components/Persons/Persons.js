4a5ad5c36a339b4916995013375f0506571a1bce
itsamanverma <amanvermame786@gmail.com>
2020-04-15 01:23:10 +0530

    using the PureComponent library of react to aboved the implementation of shouldComponentUpdate()

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Person from './Person/Person'; 
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
     constructor(props){
         super(props);
@@ -20,13 +20,12 @@ class Persons extends Component {
         console.log('[UPDATE persons.js] inside componentWillReceiveProps()', nextProps);
       }
 
-      shouldComponentUpdate (nextProps, nextState) {
-        console.log('[UPDATE Persons.js] inside shouldComponentUpdate()', nextProps, nextState);
-        return nextProps.persons !== this.props.persons ||
-               nextProps.changed !== this.props.changed ||
-               nextProps.clicked !== this.props.clicked;
-        // return true;
-      }
+      // shouldComponentUpdate (nextProps, nextState) {
+      //   console.log('[UPDATE Persons.js] inside shouldComponentUpdate()', nextProps, nextState);
+      //   return nextProps.persons !== this.props.persons ||
+      //          nextProps.changed !== this.props.changed ||
+      //          nextProps.clicked !== this.props.clicked;
+      // }
 
       componentWillUpdate (nextProps, nextState) {
         console.log('[UPDATE persons.js] inside componentWillUpdate()', nextProps, nextState);
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 
-class App extends Component {
+class App extends PureComponent {
 
     constructor(props){
       super(props);
@@ -28,12 +28,11 @@ class App extends Component {
       console.log('[App.js] inside componentDidMount()');
     }
 
-    shouldComponentUpdate (nextProps, nextState) {
-      console.log('[UPDATE App.js] inside shouldComponentUpdate()', nextProps, nextState);
-      return nextState.persons !== this.state.persons ||
-             nextState.showPersons !== this.state.showPersons;
-      // return true;
-    }
+    // shouldComponentUpdate (nextProps, nextState) {
+    //   console.log('[UPDATE App.js] inside shouldComponentUpdate()', nextProps, nextState);
+    //   return nextState.persons !== this.state.persons ||
+    //          nextState.showPersons !== this.state.showPersons;
+    // }
 
     componentWillUpdate (nextProps, nextState) {
       console.log('[UPDATE App.js] inside componentWillUpdate()', nextProps, nextState);
