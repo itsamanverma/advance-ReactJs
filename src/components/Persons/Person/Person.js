421409605a8dce649acd0af89e4b770756a635ba
itsamanverma <amanvermame786@gmail.com>
2020-04-20 01:46:25 +0530

    wrappering components with custom classes

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
+import WithClass from '../../../hoc/WithClass';
 
 class Person extends Component{
 
@@ -19,11 +20,11 @@ class Person extends Component{
         render() {
                 console.log('[Person.js] inside render()');
                 return (
-                        <div className= {classes.Person} >
+                        <WithClass classes= {classes.Person} >
                          <p onClick={this.props.click}><strong> I am {this.props.name} and I'm {this.props.age} old age</strong></p>
                          <p><i>{this.props.children}</i></p>
                          <input type="text" onChange ={this.props.changed} value={this.props.name}/>
-                        </div> 
+                        </WithClass> 
                  )
         }
 }
