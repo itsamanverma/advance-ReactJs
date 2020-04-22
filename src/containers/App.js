4f4b477cf5201b7efa7ae89cf4aa67d48b5c6bf3
itsamanverma <amanvermame786@gmail.com>
2020-04-22 23:11:20 +0530

    correct ways of mutates the State inside the SetState method

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends PureComponent {
           { id:3, name: "mini" , age:24}
         ],
         otherState: "Some other Value",
-        showPersons: false
+        showPersons: false,
+        toggleClicked: 0
       }
     }
 
@@ -84,7 +85,12 @@ class App extends PureComponent {
 
     togglePersonHandler = () => {
       const doesShow = this.state.showPersons;
-      this.setState({showPersons: !doesShow});
+      this.setState( (prevState, props) => {
+        return {
+          showPersons: !doesShow,
+          toggleClicked: prevState.toggleClicked + 1
+        }
+      });
     }
 
     render() {
